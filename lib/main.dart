1612fe4c02320891d2639768f7ede4e2456f48d8
Trinh Vu <txvu100@gmail.com>
2021-09-15 21:37:09 -0500

    Capstone-24 recreated ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:monkey_management/view/auth_view/auth_screen.dart';
 import 'package:monkey_management/view/main_screen.dart';
 import 'package:monkey_management/view/auth_view/signin_screen.dart';
 import 'package:monkey_management/view/splash_screen.dart';
@@ -15,70 +16,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    // SystemChrome.setEnabledSystemUIOverlays([]);
     // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
 
-    // Future<void> _fetchInitialData() async {
-    //   print('fetching initial data...');
-    //   // await FirebaseController.initStaffList();
-    //   await FirebaseController.fetchPermanentStaffList();
-    //   await FirebaseController.fetchWorkDay(Data.currentWorkDay);
-    //   await FirebaseController.fetchServices();
-    // }
-
     return FutureBuilder(
-
-      // Initialize FlutterFire:
         future: _initialization,
         builder: (context, appSnapshot) {
-          return MultiProvider(
-            providers: [
-            ],
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: "Monkey Management",
-              theme: ThemeData(
-                // brightness: Brightness.dark,
-                primaryColor: Colors.blue,
-                accentColor: Colors.blue,
-                primarySwatch: Colors.blue,
-                // backgroundColor: Colors.blue,
-                // accentColor: Colors.amber,
-                // accentColorBrightness: Brightness.dark,
-                // buttonTheme: ButtonTheme.of(context).copyWith(
-                //   buttonColor: Colors.amber,
-                //   textTheme: ButtonTextTheme.primary,
-                //   shape: RoundedRectangleBorder(
-                //     borderRadius: BorderRadius.circular(20),
-                //   ),
-                // ),
-              ),
-              home:
-
-              // SplashScreen(),
-
-              appSnapshot.connectionState != ConnectionState.done
-                  ? SplashScreen()
-                  : StreamBuilder(
-                  stream: FirebaseAuth.instance.authStateChanges(),
-                  builder: (ctx, userSnapshot) {
-                    if (userSnapshot.connectionState == ConnectionState.waiting) {
-                      return SplashScreen();
-                    }
-                    if (userSnapshot.hasData) {
-                      // print('hello');
-                      return MainScreen();
-                      // return FutureBuilder(
-                      //   future: _fetchInitialData(),
-                      //   builder: (context, snapshot) => TicketsScreen(),
-                      // );
-                    }
-                    return SignInScreen();
-                  }),
-              routes: {
-
-              },
-            ),
+          return MaterialApp(
+            // debugShowCheckedModeBanner: false,
+            title: "Monkey Management",
+            home: appSnapshot.connectionState != ConnectionState.done
+                ? SplashScreen()
+                : StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (ctx, userSnapshot) {
+                      if (userSnapshot.connectionState == ConnectionState.waiting) {
+                        return SplashScreen();
+                      }
+                      if (userSnapshot.hasData) {
+                        return MainScreen();
+                      }
+                      return AuthScreen();
+                    }),
+            routes: {},
           );
         });
   }
